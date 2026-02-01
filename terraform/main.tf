db78266e2176ca0582ed49fbef5c13c2a798b39c
leonel <leonel.paiva.costa@hotmail.com>
2026-01-31 20:38:41 -0500

    Add VPC, subnet and security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,85 @@
-resource "aws_instance" "devops_vm" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = var.instance_type
+# -------------------
+# VPC
+# -------------------
+resource "aws_vpc" "devops_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
-    Name = "devops-lab"
-    Environment = "dev"
+    Name = "devops-vpc"
   }
 }
 
+# -------------------
+# Subnet pública
+# -------------------
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.devops_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# -------------------
+# Internet Gateway
+# -------------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.devops_vpc.id
+
+  tags = {
+    Name = "devops-igw"
+  }
+}
+
+# -------------------
+# Route Table
+# -------------------
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.devops_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+# Associação da subnet com a route table
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# -------------------
+# AMI Amazon Linux 2023
+# -------------------
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+# -------------------
+# EC2
+# -------------------
+resource "aws_instance" "devops_vm" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "devops-vm"
+  }
+}
